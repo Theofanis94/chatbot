64a1d186529250bc1ca519f4493f9b26625fd745
Theofanis94 <fkaramitsos@outlook.com>
2022-06-07 16:55:39 +0300

    Chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chatbot from "react-chatbot-kit";
 
-function App() {
+import config from "./chatbot/config";
+import ActionProvider from "./chatbot/ActionProvider";
+import MessageParser from "./chatbot/MessageParser";
+
+import ".//App.css";
+
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{maxWidth: "3000px"}}>
+       <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+       
+       />
+
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/chatbot/MessageParser.js
@@ -0,0 +1,12 @@
+class MessageParser {
+    constructor(actionProvider, state) {
+      this.actionProvider = actionProvider;
+    }
+  
+    parse(message) {
+      console.log(message)
+    }
+  }
+  
+  export default MessageParser;
+  
